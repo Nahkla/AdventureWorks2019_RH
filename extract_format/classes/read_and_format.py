4382bbc25dad598af4597ea0a3e4097f0dc6fc0d
Sebastian Hackler <shackler@informationsfabrik.de>
2021-04-19 21:58:03 +0200

    schema csv

--- extract_format/classes/read_and_format.py
+++ extract_format/classes/read_and_format.py
@@ -3,27 +3,64 @@ from extract_format.classes import misc
 
 file_path = misc.load_rel_path(
     directory='files',
-    filename='AW_2019_Schematas',
+    filename='AW_2019_schemas',
     suffix='csv'
 )
 
-class rnf:
 
+class RnF:
     file = file_path
+    schemas = schema_table = pd.read_csv(
+        file, delimiter=';'
+    )
+    mx_array = [
+        schemas['schema'].tolist(),
+        schemas['table'].tolist()
+    ]
+    mx_tuples = list(
+        zip(*mx_array)
+    )
+    schemas_index = pd.MultiIndex.from_tuples(
+        mx_tuples,
+        names=[
+            'schema',
+            'table'
+        ]
+    )
+    schemas.index = schemas_index
 
-    def __init__(self):
-        file = self.file
+    schemas = schemas.drop(
+        [
+            'schema',
+            'table',
+        ],
+        axis=1
+    )
 
-    def load_csv(self):
-
-        schema_table = pd.read_csv(
-            self.file, delimiter=';'
-        )
-        return schema_table
-
-    def read_schemas(self):
-        return self.load_csv()['Schema'].unique()
+    def __init__(
+            self,
+            schema_out,
+            table_out
+    ):
+        self.schema_out = schema_out
+        self.table_out = table_out
 
     def format_schema(self):
-        print(self.read_schemas())
-rnf().format_schema()
+        top_line = f'Table {self.schema_out}{self.table_out}' \
+                   + ' as ' + \
+                   f'{self.schema_out[0]}{self.table_out[0]}' \
+                   + ' {'
+        self.schemas.sort_index(inplace=True)  # to get rid of performance warning line 837
+        attributes = self.schemas.loc[pd.IndexSlice[(self.schema_out, self.table_out)], 'column']
+        domains = self.schemas.loc[pd.IndexSlice[(self.schema_out, self.table_out)], 'domain']
+        bottom_line = '}'
+
+        return print(
+            top_line, *attributes, bottom_line, sep="\n"
+        )
+
+
+RnF(
+    schema_out='Person',
+    table_out='Person'
+).format_schema()
